f6399ad4a1b1fc39ed5b28610e9e2757d6a1e252
Calvin Tantio <calvin.tantio@outlook.com>
2018-07-16 14:32:55 +0800

    Added Pollutant to Chemical formula string method in Pollutants

--- BreezoModel/Model/Pollutants.swift
+++ BreezoModel/Model/Pollutants.swift
@@ -13,4 +13,23 @@ public struct Pollutants: Decodable {
     public let pm10: Pollutant
     public let pm25: Pollutant
     public let so2: Pollutant
+
+    public static func checmicalFormula(of pollutant: Pollutant, proper: Bool = true) -> String? {
+        switch pollutant.pollutant_description {
+        case "Carbon monoxide":
+            return proper ? "CO" : "co"
+        case "Nitrogen dioxide":
+            return proper ? "NO₂" : "no2"
+        case "Ozone":
+            return proper ? "O₃" : "o3"
+        case "Inhalable particulate matter (<10µm)":
+            return proper ? "PM₁₀" : "pm10"
+        case "Fine particulate matter (<2.5µm)":
+            return proper ? "PM₂.₅" : "pm25"
+        case "Sulfur dioxide":
+            return proper ? "SO₂" : "so2"
+        default:
+            return nil
+        }
+    }
 }
